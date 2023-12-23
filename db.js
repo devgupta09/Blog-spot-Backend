82e2695a90399fa03e0b1529970e1885f67c4ac2
Dev Gupta <dev2000.gupta@gmail.com>
2023-12-23 15:48:17 +0530

    name changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongooURI = process.env["MOONGODB_URI"];
+const mongooURI = process.env["DATABASE_URL"];
 
 const connectToMongo = () => {
   mongoose.connect(mongooURI);
